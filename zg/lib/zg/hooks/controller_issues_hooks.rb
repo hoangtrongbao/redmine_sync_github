969a24d8670dec9b7860761a67ccd714ab6e945e
phucdh <phucdh@zigexn.vn>
2018-01-28 22:47:02 +0700

    Update controller_issues_hooks.rb

--- zg/lib/zg/hooks/controller_issues_hooks.rb
+++ zg/lib/zg/hooks/controller_issues_hooks.rb
@@ -3,8 +3,8 @@ module Zg
     class ControllerIssuesHooks < Redmine::Hook::ViewListener
       def controller_issues_new_after_save(context={})
         issue = context[:issue]
-        client = Octokit::Client.new(login: 'hoangphucd3@gmail.com',
-                                     password: 'phuc142536')
+        client = Octokit::Client.new(login: '',
+                                     password: '')
 
         client.create_issue('hoangphucd3/test_jenkins',
                             issue.subject,
