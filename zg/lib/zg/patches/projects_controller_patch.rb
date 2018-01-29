1406b59e4bfefe66121f8c729a3655d032efaa95
Trong Bao Hoang <baoht@zigexn.vn>
2018-01-29 13:32:03 +0700

    Update save git_repo_name column

--- zg/lib/zg/patches/projects_controller_patch.rb
+++ zg/lib/zg/patches/projects_controller_patch.rb
@@ -4,17 +4,19 @@ module Zg
       def self.included(base)
         base.send(:include, InstanceMethods)
         base.class_eval do
-          after_action :save_git_repo_url, only: %i[create update]
+          after_action :build_git_repo_url, only: %i[create update]
         end
       end
 
       module InstanceMethods
         def build_git_repo_url
           git_repo_url = params[:project][:git_repo_url]
+          git_repo_name = git_repo_url.split('github.com')[1] if git_repo_url.present?
           return if @project.errors.any? || git_repo_url.blank?
           # @TODO: Check if ventura project exist?
           VenturaProject.create(project: @project,
-                                git_repo_url: git_repo_url)
+                                git_repo_url: git_repo_url,
+                                git_repo_name: git_repo_name)
         end
       end
     end
