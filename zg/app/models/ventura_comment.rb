87e24cd86237e5a33b734964ed368fd0e9867ce0
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-29 12:58:14 +0700

    Sync update comment

--- zg/app/models/ventura_comment.rb
+++ zg/app/models/ventura_comment.rb
@@ -1,10 +1,12 @@
 class VenturaComment < ActiveRecord::Base
   belongs_to :journal, foreign_key: :journal_id
-  after_destroy :sync_github_comment
+
+  after_update :update_github_comment
+  after_destroy :delete_github_comment
 
   private
 
-  def sync_github_comment
+  def delete_github_comment
     user = User.current
     oauth_token = user.ventura_user.oauth_token
     client = Octokit::Client.new(access_token: oauth_token)
--- zg/lib/zg/patches/journal_patch.rb
+++ zg/lib/zg/patches/journal_patch.rb
@@ -5,11 +5,19 @@ module Zg
         base.send(:include, InstanceMethods)
         base.class_eval do
           has_one :ventura_comment, dependent: :destroy
+          after_update :update_github_comment
         end
       end
 
       module InstanceMethods
-
+        def update_github_comment
+          user = User.current
+          oauth_token = user.ventura_user.oauth_token
+          client = Octokit::Client.new(access_token: oauth_token)
+          client.update_comment('phucdh/test_redmine',
+                                ventura_comment.git_comment_id,
+                                notes)
+        end
       end
     end
   end
