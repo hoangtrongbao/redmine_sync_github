9504c0275ce6568e5a528c40f9c986c87974b957
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-30 16:54:20 +0700

    Remove action hook model comment

--- zg/app/models/ventura_comment.rb
+++ zg/app/models/ventura_comment.rb
@@ -1,14 +1,4 @@
 class VenturaComment < ActiveRecord::Base
   unloadable
   belongs_to :journal, foreign_key: :journal_id
-
-  after_update :update_github_comment
-  after_destroy :delete_github_comment
-
-  private
-
-  def delete_github_comment
-    repo = journal.issue.project.ventura_project.git_repo_name
-    Zg::GithubAdapter.new(repo).delete_comment(git_comment_id)
-  end
 end
