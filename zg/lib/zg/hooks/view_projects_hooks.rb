6956cc85ec8c8cd75856c00158bc2fa0d8a5d20d
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-30 10:46:35 +0700

    Add github notify in project

--- zg/lib/zg/hooks/view_projects_hooks.rb
+++ zg/lib/zg/hooks/view_projects_hooks.rb
@@ -2,6 +2,7 @@ module Zg
   module Hooks
     class ViewProjectHooks < Redmine::Hook::ViewListener
       render_on :view_projects_form, partial: 'projects/git_repo'
+      render_on :view_projects_show_right, partial: 'projects/zg_notify'
     end
   end
 end
