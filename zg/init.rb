a7984168f7d47a1603c840b5dd78d576fc9de111
Hoang Phuc Do <phucdh@zigexn.vn>
2018-01-31 09:32:40 +0700

    Move require to zg.rb

--- zg/init.rb
+++ zg/init.rb
@@ -7,23 +7,8 @@ Redmine::Plugin.register :zg do
   version '0.0.1'
   url ''
   author_url ''
-
-  # menu :account_menu,
-  #      :zg,
-  #      { controller: 'user_oauth', action: 'index'},
-  #      caption: 'Github OAuth'
 end
 
 ActionDispatch::Callbacks.to_prepare do
-  require 'zg/hooks/controller_issues_hooks'
-  require 'zg/hooks/controller_journals_hooks'
-  # require 'zg/hooks/view_welcome_hooks'
-  require 'zg/hooks/view_my_hooks'
-  require 'zg/hooks/view_projects_hooks'
-  require 'zg/patches/project_patch'
-  require 'zg/patches/projects_controller_patch'
-  require 'zg/patches/user_patch'
-  require 'zg/patches/issue_patch'
-  require 'zg/patches/journal_patch'
-  require 'zg/patches/users_helper_patch'
+  require 'zg'
 end
--- zg/lib/zg/hooks/view_welcome_hooks.rb
+++
@@ -1,7 +0,0 @@
-module Zg
-  module Hooks
-    class ViewWelcomeHooks < Redmine::Hook::ViewListener
-      # render_on :view_welcome_index_left, partial: 'welcome/oauth'
-    end
-  end
-end
